c226d8e84229f03172818181ca47ea96d3543229
sibi <selvimuthusamy785@gmail.com>
2024-11-15 17:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def hello_world():
+    return "Hello, World!"
